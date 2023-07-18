bfc50cbfbdd69621481a102bcf4cf232e48aa66f
Oviedo138 <c.oviedo@dappertechnologies.com>
2023-07-18 17:39:11 -0500

    Add header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  showMenu: boolean = false;
+  rutas: string[]=['Inicio', 'Compra', 'Renta', 'Inmuebles destacados', '¿Quienes somos?', 'Lo que dicen de nosotros', 'Contacto']
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
